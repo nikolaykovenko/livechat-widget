33cdd4ebbf002ee096d0ada5f485a51fcadeb359
Nickk <nickk@templatemonster.me>
2018-09-11 09:10:10 +0200

    Added init()

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ $(function() {
         const greeting = new Greeting();
         console.log(greeting.getRandom());
 
+        const LiveChat = LiveChat || {};
+        LiveChat.init();
         LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
     });
 });
