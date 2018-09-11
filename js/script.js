935f767d1bdc9fc41a7ae887a6e1a94d73dfabc4
Nickk <nickk@templatemonster.me>
2018-09-11 10:22:21 +0200

    Just simple

--- js/script.js
+++ js/script.js
@@ -5,13 +5,6 @@ LiveChat.on('customer_profile', function( data ) {
     console.log(data);
 });
 
-$(function() {
-    $('#hello-button').click(function () {
-        const greeting = new Greeting();
-        console.log(greeting.getRandom());
+LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
+LiveChat.getSessionId()
 
-        console.log(LiveChat);
-        console.log(LiveChat.getSessionId());
-        LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
-    });
-});
