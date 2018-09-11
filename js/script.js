b15db0d4d7ec35cf232572eb3d28945a70350f7f
Nickk <nickk@templatemonster.me>
2018-09-11 10:16:41 +0200

    Debug

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-var LiveChat = LiveChat || {};
 LiveChat.init();
+
+// listen to customer profile change event from LiveChat Agent App
+LiveChat.on('customer_profile', function( data ) {
+    console.log(data);
+});
 
 $(function() {
     $('#hello-button').click(function () {
@@ -7,8 +11,6 @@ $(function() {
         console.log(greeting.getRandom());
 
         console.log(LiveChat);
-        console.log(LiveChat.getSessionId());
-
         console.log(LiveChat.getSessionId());
         LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
     });
