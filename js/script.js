700039c2634e0fb8c3c6e5b46693648b6549749e
Nickk <nickk@templatemonster.me>
2018-09-11 09:46:06 +0200

    Test

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-var LiveChat = LiveChat || {};
-LiveChat.init();
-
-console.log(LiveChat);
-console.log(LiveChat.getSessionId());
 
 $(function() {
     $('#hello-button').click(function () {
         const greeting = new Greeting();
         console.log(greeting.getRandom());
+
+
+        var LiveChat = LiveChat || {};
+        LiveChat.init();
+
+        console.log(LiveChat);
+        console.log(LiveChat.getSessionId());
 
         console.log(LiveChat.getSessionId());
         LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
