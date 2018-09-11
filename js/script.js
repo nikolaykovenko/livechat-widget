3dd1e9286d911157d2dfc41ca4a9cab1e8004688
Nickk <nickk@templatemonster.me>
2018-09-11 09:18:25 +0200

    postMessage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 LiveChat.init();
 
-LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
+LiveChat.postMessage("Hello! This message comes from the App Widget. Press enter to send it!");
 
 $(function() {
     $('#hello-button').click(function () {
