f75b5301e0ae763034fa9ec0163ec9ee1af155df
Nickk <nickk@templatemonster.me>
2018-09-11 09:12:59 +0200

    Trying to use Livechat object

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+const LiveChat = LiveChat || {};
+LiveChat.init();
+
+LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
+
 $(function() {
     $('#hello-button').click(function () {
         const greeting = new Greeting();
         console.log(greeting.getRandom());
-
-        const LiveChat = LiveChat || {};
-        LiveChat.init();
-        LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
     });
 });
