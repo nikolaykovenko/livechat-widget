fc78e0c8fe6ee632bc59faa6121781989759e833
Nickk <nickk@templatemonster.me>
2018-09-11 11:49:46 +0200

    Added greetings

--- js/script.js
+++ js/script.js
@@ -7,11 +7,7 @@ LiveChat.on('customer_profile', function( data ) {
 
 $(function() {
     $('#hello-button').click(function () {
-        // const greeting = new Greeting();
-        // console.log(greeting.getRandom());
-
-        console.log(LiveChat);
-        console.log(LiveChat.getSessionId());
-        LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
+        const greeting = new Greeting();
+        LiveChat.putMessage(greeting.getRandom());
     });
 });
