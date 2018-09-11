ee70c52af922adba9c65eeb6035235a0221d944b
Nickk <nickk@templatemonster.me>
2018-09-11 09:42:51 +0200

    LiveChat.getSessionId()

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ var LiveChat = LiveChat || {};
 LiveChat.init();
 
 console.log(LiveChat);
-
 console.log(LiveChat.getSessionId());
-LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
 
 $(function() {
     $('#hello-button').click(function () {
         const greeting = new Greeting();
         console.log(greeting.getRandom());
+
+        console.log(LiveChat.getSessionId());
+        LiveChat.putMessage("Hello! This message comes from the App Widget. Press enter to send it!");
     });
 });
